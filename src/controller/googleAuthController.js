fd3ec6d9f85637c8340576be190b0ac357923245
Gaurav kumar <gk064577@gmail.com>
2026-04-23 23:03:17 +0530

    updated logout  code for prod

--- src/controller/googleAuthController.js
+++ src/controller/googleAuthController.js
@@ -1,67 +1,66 @@
-import User from "../schema/userSchema.js"
-import {generateToken} from "../utils/generateToken.js"
+import User from "../schema/userSchema.js";
+import { generateToken } from "../utils/generateToken.js";
 import { OAuth2Client } from "google-auth-library";
-import axios from "axios"
+import axios from "axios";
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
-export const googleAuth=(req,res)=>{
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?` +
+export const googleAuth = (req, res) => {
+  const url =
+    `https://accounts.google.com/o/oauth2/v2/auth?` +
     `client_id=${process.env.CLIENT_ID}` +
     `&redirect_uri=${process.env.REDIRECT_URI}` +
     `&response_type=code` +
     `&scope=openid email profile`;
 
-  console.log(url+"from url get req.....................")
+  console.log(url + "from url get req.....................");
   res.redirect(url);
+};
 
-}
+export const googleCallback = async (req, res) => {
+  try {
+    const code = req.query.code;
+    //exchange code
+    const tokenRes = await axios.post("https://oauth2.googleapis.com/token", {
+      code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URI,
+      grant_type: "authorization_code",
+    });
+    console.log(
+      tokenRes,
+      "________________________________________________________________________",
+    );
 
-export const googleCallback=async(req,res)=>{
- try {
-    const code=req.query.code;
-    //exchange code 
-    const tokenRes=await axios.post("https://oauth2.googleapis.com/token",
-        {
-        code,
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        redirect_uri: process.env.REDIRECT_URI,
-        grant_type: "authorization_code",
-        })
-        console.log(tokenRes,"________________________________________________________________________")
-        
-        const{id_token}=tokenRes.data
-        
-  
+    const { id_token } = tokenRes.data;
 
     //verify id token
     const ticket = await client.verifyIdToken({
-       idToken: id_token,
-       audience: process.env.CLIENT_ID,
+      idToken: id_token,
+      audience: process.env.CLIENT_ID,
     });
 
-    const googleUser=ticket.getPayload();
-    console.log(googleUser,"From google callback")
+    const googleUser = ticket.getPayload();
+    console.log(googleUser, "From google callback");
 
     //creating user in db
-    let user=await User.findOne({providerID:googleUser.sub})
+    let user = await User.findOne({ providerID: googleUser.sub });
 
-    if(!user){
-        user=await User.create({
-            name:googleUser.name,
-            email:googleUser.email,
-            profile:googleUser.picture,
-            providerID:googleUser.sub,
-        })   
-    }
-    else{
-        user.lastLoggedIN = new Date();
-        await user.save();
+    if (!user) {
+      user = await User.create({
+        name: googleUser.name,
+        email: googleUser.email,
+        profile: googleUser.picture,
+        providerID: googleUser.sub,
+      });
+    } else {
+      user.lastLoggedIN = new Date();
+      await user.save();
     }
 
     //jwt token
-    const token=generateToken(user._id)
+    const token = generateToken(user._id);
 
     res.cookie("token", token, {
       httpOnly: true,
@@ -74,22 +73,32 @@ export const googleCallback=async(req,res)=>{
 
     //redirect now for developement only
     //  res.redirect("http://localhost:5173/dashboard");
-     res.redirect("https://prappers.pages.dev/dashboard");
+    res.redirect("https://prappers.pages.dev/dashboard");
+  } catch (error) {
+    console.log(error + "Error from google callback controller");
+    // res.redirect("http://localhost:5173/login");
+    res.redirect("https://prappers.pages.dev/login");
+  }
+};
 
-
- }catch(error){
-    console.log(error+"Error from google callback controller")
-     // res.redirect("http://localhost:5173/login");
-     res.redirect("https://prappers.pages.dev/login");
- }
-}
-
-export const getMe=async (req,res)=>{
+export const getMe = async (req, res) => {
   const user = await User.findById(req.user.userId);
   res.json(user);
-}
+};
 
-export const logOut=(req,res)=>{
-   res.clearCookie("token")
-   res.json({message:"Logged out successfully"})
-}
+// export const logOut=(req,res)=>{
+//    res.clearCookie("token")
+//    res.json({message:"Logged out successfully"})
+// }
+
+export const logOut = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    domain: ".kyzron.com",
+    expires: new Date(0), // 🔥 ensures deletion
+  });
+
+  res.json({ message: "Logged out successfully" });
+};
